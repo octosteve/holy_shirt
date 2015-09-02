5cb41ddb7585ee5bb879826c636ad3c175850f53
Steven Nunez <steven.nunez@gmail.com>
2015-09-02 16:40:22 -0400

    Manage shirts

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -2,4 +2,41 @@ class ShirtsController < ApplicationController
   def index
     @shirts = Shirt.all
   end
+
+  def new
+    @shirt = Shirt.new
+  end
+
+  def create
+    @shirt = Shirt.new(safe_shirt)
+    if @shirt.save
+      flash[:notice] = "You successfully create a shirt! ZOMG"
+      redirect_to @shirt
+    else
+      render :new
+    end
+  end
+
+  def show
+    @shirt = Shirt.find(params[:id])
+  end
+
+  def edit
+    @shirt = Shirt.find(params[:id])
+  end
+
+  def update
+    @shirt = Shirt.find(params[:id])
+    if @shirt.update(shirt_params)
+      flash[:notice] = "#{@shirt.name} has been updated"
+      redirect_to @shirt
+    else
+      render :edit
+    end
+  end
+
+  private
+  def shirt_params
+    params.require(:shirt).permit(:name, :artist, :image_url, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "home#index"
   get 'random_shirt', to: "random_shirt#index"
   get 'search', to: 'search#index'
-  resources :shirts, only: [:index]
+  resources :shirts, except: [:destroy]
   resources :tags, only: [:index, :edit]
 end
