7670bd4f97bf90f5635b85bb038c66a604509468
Steven Nunez <steven.nunez@gmail.com>
2015-09-04 11:38:14 -0400

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902204136) do
+ActiveRecord::Schema.define(version: 20150904153157) do
 
   create_table "classifications", force: :cascade do |t|
     t.integer  "shirt_id"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20150902204136) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
