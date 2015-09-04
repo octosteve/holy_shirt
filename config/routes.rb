6eb80aab65a8a7c4e6d0bbe3c684698bf980c8cf
Steven Nunez <steven.nunez@gmail.com>
2015-09-04 12:20:34 -0400

    Add Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'registrations/new'
+
   root "home#index"
-  get 'random_shirt', to: "random_shirt#index"
-  get 'search', to: 'search#index'
   resources :shirts, except: [:destroy]
   resources :tags, only: [:index, :edit]
+  get 'random_shirt', to: "random_shirt#index"
+  get 'search', to: 'search#index'
+
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
 end
